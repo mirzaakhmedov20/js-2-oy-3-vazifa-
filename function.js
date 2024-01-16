460391e817602d0900cd7c40e2a8696454c6f9be
mirzaakhmedov20 <mirzaakhmedovshukurullo20gmail.com>
2024-01-16 21:54:09 +0500

    Homework 2month

---
+++ function.js
@@ -0,0 +1,27 @@
+// Quyidagi kabi massiv berilganda uning elementlaridan iborat cardlar yasash.
+// Cardlarni korinishi rasm holatida yuklangan.
+
+// Bundan tashqari card larda delete butoon mavjud. Ushbu button bosilganda sahifadan ushbu 
+// cardni o'chirib tashlashi kerak bo'ladi. O'chirishda ham confirm modaldan foydalanib oldin
+// haqiqatdan ochirmoqchi ekanini sorab olinishi zarur.
+
+const data = [
+     {
+          id: "1",
+          name: "BMW",
+          price: 35000,
+          description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, quam."
+     },
+     {
+          id: "2",
+          name: "Mercedes",
+          price: 40000,
+          description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, quam."
+     },
+     {
+          id: "3",
+          name: "Tesla",
+          price: 22000,
+          description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, quam."
+     },
+];
